e662679e022e203177c512476c57d0e6ed912054
roblivesinottawa <tech.rob@icloud.com>
2021-03-09 15:38:26 -0500

    "second commit"

--- server/app.js
+++ server/app.js
@@ -13,6 +13,13 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
 
+// handle production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'))
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 
 const port = process.env.PORT || 5000
 
